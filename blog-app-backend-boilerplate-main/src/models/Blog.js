9c0d69e418db85a771295f9c0b9a527e6bad9d00
narasimha <lakshminarasimha72692@gmail.com>
2023-09-04 16:22:01 +0530

    Assignment-6 added

---
+++ blog-app-backend-boilerplate-main/src/models/Blog.js
@@ -0,0 +1,10 @@
+const mongooose = require('mongoose');
+const blogSchema = new mongooose.Schema({
+    // Your code goes here
+    topic: String,
+    description: String,
+    posted_at: Date,
+    posted_by: String
+})
+const Blog = mongooose.model('blogs', blogSchema);
+module.exports = Blog;
